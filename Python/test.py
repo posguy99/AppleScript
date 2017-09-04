ae37b97086b5930f828d2c14d8d8d5621896bcec
Marc Wilson <posguy99@gmail.com>
2017-09-04 08:45:13 -0700

    Initial commit

---
+++ Python/test.py
@@ -0,0 +1,23 @@
+
+# dr drang appleScript handler for python
+# http://www.leancrew.com/all-this/2013/03/combining-python-and-applescript/
+
+from applescript import asrun, asquote
+
+subject = 'A new email'
+
+body = '''This is the body of my "email."
+I hope it comes out right.
+
+Regards,
+Dr. Drang
+'''
+ascript = '''
+tell application "Mail"
+  activate
+  make new outgoing message with properties {{visible:true, subject:{0}, content:{1}}}
+end tell
+'''.format(asquote(subject), asquote(body))
+
+print ascript
+asrun(ascript)

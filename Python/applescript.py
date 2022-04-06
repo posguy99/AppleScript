ef305d9f148382d65e117f2fef6f901a57aa7921
Marc Wilson <posguy99@gmail.com>
2022-04-05 21:08:29 -0700

    fixup to avoid MemoryView error in osa.communicate call

--- Python/applescript.py
+++ Python/applescript.py
@@ -10,7 +10,7 @@ def asrun(ascript):
   osa = subprocess.Popen(['osascript', '-'],
                          stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE)
-  return osa.communicate(ascript)[0]
+  return osa.communicate(bytes(ascript,'UTF-8'))[0]
 
 def asquote(astr):
   "Return the AppleScript equivalent of the given string."
